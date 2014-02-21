38a0ab0caba4be059954129fba147959b6ad0268
AlsekseiNovitskiy <mr.novik@gmail.com>
2014-02-21 17:13:12 +0200

    FOSUserBundle implemented

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Pkonekt\UserBundle\PkonektUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/Pkonekt/UserBundle/PkonektUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Pkonekt\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class PkonektUserBundle extends Bundle
+{
+}
